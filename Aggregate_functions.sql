e82f7677e8aaf25fe9387005dab6dc94dffc3ae9
shweta <shweta.borganve@gmail.com>
2026-06-27 00:28:07 +0530

    Added Aggregate_functions.sql shows how to use SQL aggregate functions to summarize data

---
+++ Aggregate_functions.sql
@@ -0,0 +1,25 @@
+create database college;
+use college;
+
+create table student (
+rollno int primary key,
+name varchar(50),
+marks int not null,
+grade varchar(1),
+city varchar(20)
+);
+
+insert into student (rollno, name, marks, grade, city)
+values ( 112, "anil", 78, "C", "Pune"),
+(113, "bhumika", 93, "A", "Mumbai"),
+(114, "aakash", 76, "C", "Mole"),
+(115, "swati", 85, "B", "Mumbai"),
+(116, "pooja", 96, "A", "Ainapur"),
+(117, "aish", 12, "F", "Mole"),
+(118, "Tanvi", 82, "B", "Ainapur");
+
+select*from student;
+select MAX(marks) from student;
+select MIN(marks) from student;
+select COUNT(rollno) from student;
+select AVG(marks) from student;
