bf652cb5d4a3018a4f16ffa78af78a1742f2e576
shweta <shweta.borganve@gmail.com>
2026-06-27 00:16:30 +0530

    Added OrderBy_Clause.sql used in SQL to sort the rows returned by a query according to one or more columns

---
+++ OrderBy_Clause.sql
@@ -0,0 +1,24 @@
+create database college;
+use college;
+
+create table student (
+rollno int primary key,
+name varchar(50),
+marks int not null,
+grade varchar(1),
+city varchar(20)
+);
+
+insert into student (rollno, name, marks, grade, city)
+values ( 112, "anil", 78, "C", "Pune"),
+(113, "bhumika", 93, "A", "Mumbai"),
+(114, "aakash", 76, "C", "Mole"),
+(115, "swati", 85, "B", "Mumbai"),
+(116, "pooja", 96, "A", "Ainapur"),
+(117, "aish", 12, "F", "Mole"),
+(118, "Tanvi", 82, "B", "Ainapur");
+
+select*from student;
+select*from student order by marks ASC;
+select*from student order by marks DESC;
+select*from student order by marks DESC Limit 6;
