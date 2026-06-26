db92d7c6c2e224555332f4ffb4569df906c6c0c6
shweta <shweta.borganve@gmail.com>
2026-06-26 16:36:01 +0530

    Added where_clause.sql The WHERE clause is used to filter rows returned by a SQL query so only records that meet specified conditions are selected. It lets you retrieve only the data you need instead of the entire table

---
+++ where_clause.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE college;
+USE college;
+
+create table student (
+rollno int primary key,
+name varchar(50),
+marks int not null,
+grade varchar(1),
+city varchar(20)
+);
+
+insert into student (rollno, name, marks, grade, city)
+values ( 112, "anil", 78, "C", "Pune"),
+(113, "bhumika", 93, "A", "Mumbai"),
+(114, "aakash", 76, "C", "Mole"),
+(115, "swati", 85, "B", "Mumbai"),
+(116, "pooja", 96, "A", "Ainapur"),
+(117, "aish", 12, "F", "Mole"),
+(118, "Tanvi", 82, "B", "Ainapur"); 
+
+select name, marks from student;
+select* from student;
+select city from student;
+select DISTINCT city from student;
